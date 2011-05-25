krell/tg-elysium-xiii

commit 67935cc550577cf5181d960bb593f0e3681990ab

Twohanded weapons are now more generalized. You can make any weapon twohanded just by setting twohanded = 1. Also fixed the throwing of twohanded weapons as well as dropping. The offhand thing should no longer cause problems. Report any bugs.
git-svn-id: http://tgstation13.googlecode.com/svn/trunk@1615 316c924e-a436-60f5-8080-3fe189b3f50e

code/modules/mob/mob.dm
@@ -1134,7 +1134,7 @@ proc/isobserver(A)
 
 /mob/proc/drop_item()
 	var/obj/item/W = equipped()
-	if(istype(W,/obj/item/weapon/fireaxe))
+	if(W.twohanded)
 		if(W.wielded)
 			if(hand)
 				var/obj/item/weapon/offhand/O = r_hand
@@ -1143,7 +1143,7 @@ proc/isobserver(A)
 				var/obj/item/weapon/offhand/O = l_hand
 				del O
 		W.wielded = 0          //Kinda crude, but gets the job done with minimal pain -Agouri
-		W.name = "Fire axe" //name reset so people don't see world fireaxes with (unwielded) or (wielded) tags
+		W.name = "[initial(W.name)]" //name reset so people don't see world fireaxes with (unwielded) or (wielded) tags
 		W.update_icon()
 
 	if (W)

code/defines/obj.dm
@@ -347,6 +347,8 @@
 	var/w_class = 3.0
 	var/wielded = 0
 	var/twohanded = 0 ///Two handed and wielded off by default, nyoro~n -Agouri
+	var/force_unwielded = 0
+	var/force_wielded = 0
 	flags = FPRINT | TABLEPASS
 	pressure_resistance = 50
 	var/obj/item/master = null

code/defines/obj/weapon.dm
@@ -514,6 +514,8 @@
 	force = 5
 	w_class = 4.0
 	twohanded = 1
+	force_unwielded = 5
+	force_wielded = 30
 
 
 /obj/item/weapon/disk

code/game/objects/items/item.dm
@@ -159,6 +159,38 @@
 			src.loc = P
 			O.amount -= 1
 
+/obj/item/attack_self(mob/user as mob)
+	..()
+	if(twohanded)
+		if(wielded) //Trying to unwield it
+			wielded = 0
+			force = force_unwielded
+			src.name = "[initial(name)] (Unwielded)"
+			src.update_icon() //If needed by the particular item
+			user << "\blue You are now carrying the [initial(name)] with one hand."
+
+			if(istype(user.get_inactive_hand(),/obj/item/weapon/offhand))
+				del user.get_inactive_hand()
+			return
+		else //Trying to wield it
+			if(user.get_inactive_hand())
+				user << "\red You need your other hand to be empty"
+				return
+			wielded = 1
+			force = force_wielded
+			src.name = "[initial(name)] (Wielded)"
+			src.update_icon() //If needed by the particular item
+			user << "\blue You grab the [initial(name)] with both hands, It is ready for use."
+
+			var/obj/item/weapon/offhand/O = new /obj/item/weapon/offhand(user) ////Let's reserve his other hand~
+			O.name = text("[initial(src.name)] - Offhand")
+			O.desc = "Your second grip on the [initial(src.name)]"
+			if(user.hand)
+				user.r_hand = O          ///Place dat offhand in the opposite hand
+			else
+				user.l_hand = O
+			O.layer = 20
+			return
 
 /obj/item/proc/attack(mob/living/M as mob, mob/living/user as mob, def_zone)
 
@@ -339,7 +371,6 @@
 		M.updatehealth()
 	src.add_fingerprint(user)
 	return 1
-
 
 /obj/item/proc/eyestab(mob/living/carbon/M as mob, mob/living/carbon/user as mob)
 

code/game/objects/items/weapons/twohanded.dm
@@ -25,45 +25,4 @@
 //welp, all is good, now to see if he's trying do twohandedly wield it or unwield it
 
 
-	if(src.wielded == 0) //He's trying to wield it!!
-
-		if( user.hand ) ///User hand appears to be 1 if you're using the Left one and 0 if you're using the right? I don't even KNOW
-			if(user.r_hand != null)
-				user << "\red You need your right hand to be empty"
-				return
-		else
-			if(user.l_hand != null)
-				user << "\red You need your left hand to be empty"
-				return
-		user << "\blue You grab the hilt of the axe and raise it to your torso, readying it for use."
-		src.wielded = !src.wielded
-		src.force = 30
-		src.name = "Fire axe (Wielded)"
-		src.update_icon()
-
-		var/obj/item/weapon/offhand/O = new /obj/item/weapon/offhand(user) ////Let's reserve his other hand~
-		O.name = text("[]-Offhand",src.name)
-		O.desc = "Your second grip on the Fire axe"
-
-		if(user.hand)
-			user.r_hand = O          ///Place dat offhand in the opposite hand
-		else
-			user.l_hand = O
-		O.layer = 20
-		return
-
-
-	if(src.wielded == 1) //Guy's bored of robusting and now wants to carry it
-		src.wielded = !src.wielded
-		src.force = 5
-		src.name = "Fire axe (Unwielded)"
-		src.update_icon()
-		user << "\blue You are now carrying the axe with one hand."
-
-		if(user.hand)  //Now let's free up his other hand
-			var/obj/item/weapon/offhand/O = user.r_hand
-			del O
-		else
-			var/obj/item/weapon/offhand/O = user.l_hand
-			del O
-		return
+	..()

code/game/throwing.dm
@@ -14,6 +14,7 @@
 
 /mob/living/carbon/proc/throw_item(atom/target)
 	src.throw_mode_off()
+
 	if(usr.stat)
 		return
 	if(target.type == /obj/screen) return
@@ -21,6 +22,19 @@
 	var/atom/movable/item = src.equipped()
 
 	if(!item) return
+
+	if(istype(item,/obj/item))
+		var/obj/item/IT = item
+		if(IT.twohanded)
+			if(IT.wielded)
+				if(hand)
+					var/obj/item/weapon/offhand/O = r_hand
+					del O
+				else
+					var/obj/item/weapon/offhand/O = l_hand
+					del O
+			IT.name = initial(IT.name)
+
 
 	u_equip(item)
 	if(src.client)

commit 77c28437ac615ffa04b187e2105f8eee8d70f35e

Fixing a runtime error introduced in r1615.
Note: Checking a null object with istype won't throw up a runtime error but checking a var of a null object will.

git-svn-id: http://tgstation13.googlecode.com/svn/trunk@1616 316c924e-a436-60f5-8080-3fe189b3f50e

code/modules/mob/mob.dm
@@ -1134,19 +1134,19 @@ proc/isobserver(A)
 
 /mob/proc/drop_item()
 	var/obj/item/W = equipped()
-	if(W.twohanded)
-		if(W.wielded)
-			if(hand)
-				var/obj/item/weapon/offhand/O = r_hand
-				del O
-			else
-				var/obj/item/weapon/offhand/O = l_hand
-				del O
-		W.wielded = 0          //Kinda crude, but gets the job done with minimal pain -Agouri
-		W.name = "[initial(W.name)]" //name reset so people don't see world fireaxes with (unwielded) or (wielded) tags
-		W.update_icon()
 
 	if (W)
+		if(W.twohanded)
+			if(W.wielded)
+				if(hand)
+					var/obj/item/weapon/offhand/O = r_hand
+					del O
+				else
+					var/obj/item/weapon/offhand/O = l_hand
+					del O
+			W.wielded = 0          //Kinda crude, but gets the job done with minimal pain -Agouri
+			W.name = "[initial(W.name)]" //name reset so people don't see world fireaxes with (unwielded) or (wielded) tags
+			W.update_icon()
 		u_equip(W)
 		if (client)
 			client.screen -= W